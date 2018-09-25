6f65beb4b8da04a5e735892c8f21161e69de2141
ang1106 <ang1106@naver.com>
2018-09-25 18:01:46 +0900

    9/25 - young

--- routes/index.js
+++ routes/index.js
@@ -130,7 +130,7 @@ router.post('/notice/add', function (req, res) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     } else {
-      res.redirect('/notice');
+      res.redirect('/notice/' + rows.insertId);
     }
   });
 });
